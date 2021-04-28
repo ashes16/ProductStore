0cb4f462274738e811db3add795338999d0c632e
dashaanan <0dashaanan@gmail.com>
2021-04-28 23:13:02 +0530

    Added a simple API

---
+++ PractProject/PractProject/Controllers/PersonController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PractProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PractProject.Controllers
+{
+    public class PersonController : Controller
+    {
+        [Route("api/Person")]
+        [HttpPost]
+        public ActionResult Person([FromBody] Person person)
+        {
+            Console.WriteLine(person.First_Name);
+
+            //do stuff
+            return Content(person.First_Name);
+        }
+
+    }
+}
---
+++ PractProject/PractProject/Models/Person.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PractProject.Models
+{
+    public class Person
+    {
+        public int Person_Id { get; set; }
+        public string First_Name { get; set; }
+        public string Middle_Name { get; set; }
+        public string Last_Name { get; set; }
+        public DateTime Date_Of_Birth { get; set; }
+        public string Marital_Status { get; set; }
+        public string Gender { get; set; }
+        public string Created_By { get; set; }
+        public string Modified_By { get; set; }
+        public DateTime Created_Date { get; set; }
+        public DateTime Modified_Date { get; set; }
+    }
+}
